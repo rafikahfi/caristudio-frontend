015a521581d036e506c7f3d4b4b4aaa719caeeda
Rafi Kahfi Yugi <rafikahfi01@gmail.com>
2025-07-13 18:56:25 +0700

    Memperbaiki tulisan pada Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <div className="bg-merah w-full">
-      <div className="px-6 sm:px-10 py-10 text-white text-center">
-        <p className="text-base sm:text-lg">
+      <div className="px-6 sm:px-10 py-8 text-white text-center">
+        <p className="text-sm sm:text-base">
           &copy;2025 <span className="font-oswald">CariStudio</span>. apiw. All Rights Reserved.
         </p>
       </div>
