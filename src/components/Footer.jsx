e589e2b199e80903757528057b57d85ae2bc7496
Rafi Kahfi Yugi <rafikahfi01@gmail.com>
2025-07-13 18:46:47 +0700

    Last memperbaiki Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 function Footer() {
   return (
-    <div className="bg-merah w-full">
-      <div className="flex items-center justify-center px-6 sm:px-10 py-6 text-white text-center">
+    <footer className="bg-merah w-full">
+      <div className="max-w-screen-xl mx-auto px-6 sm:px-10 py-6 text-white text-center">
         <p>
           &copy;2025 <span className="font-oswald">CariStudio</span>. apiw. All Rights Reserved.
         </p>
       </div>
-    </div>
+    </footer>
   );
 }
 
