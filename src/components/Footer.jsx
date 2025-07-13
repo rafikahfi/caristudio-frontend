ee26a6b6eefcd789920ea83d314b932eaa5f9d18
Rafi Kahfi Yugi <rafikahfi01@gmail.com>
2025-07-13 19:02:21 +0700

    Memperbaiki CSS pada Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <div className="bg-merah w-full">
-      <div className="px-6 sm:px-10 py-8 text-white text-center">
-        <p className="text-sm sm:text-base">
+      <div className="max-w-7xl mx-auto px-4 py-10 sm:py-12 text-white text-center">
+        <p className="text-base sm:text-lg">
           &copy;2025 <span className="font-oswald">CariStudio</span>. apiw. All Rights Reserved.
         </p>
       </div>
