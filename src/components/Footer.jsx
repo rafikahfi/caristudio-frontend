fd149f75564cb5e98c948a345377bf1a299d2948
Rafi Kahfi Yugi <rafikahfi01@gmail.com>
2025-07-13 18:32:39 +0700

    Memperbaiki Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 function Footer() {
   return (
-    <div className="bg-merah p-15 text-white text-center">
-      <p>
-        &copy;2025 <span className="font-oswald">CariStudio</span>. apiw. All Rights Reserved.
-      </p>
+    <div className="bg-merah w-full">
+      <div className="max-w-7xl mx-auto px-4 py-6 text-white text-center">
+        <p>
+          &copy;2025 <span className="font-oswald">CariStudio</span>. apiw. All Rights Reserved.
+        </p>
+      </div>
     </div>
   );
 }
