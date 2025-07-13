fab6607c59152d2363a11eefda0c89f5aedf9912
Rafi Kahfi Yugi <rafikahfi01@gmail.com>
2025-07-13 18:49:20 +0700

    Last inimah raul buat Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 function Footer() {
   return (
     <footer className="bg-merah w-full">
-      <div className="max-w-screen-xl mx-auto px-6 sm:px-10 py-6 text-white text-center">
-        <p>
-          &copy;2025 <span className="font-oswald">CariStudio</span>. apiw. All Rights Reserved.
-        </p>
+      <div className="max-w-screen-xl mx-auto px-6 sm:px-10 py-6">
+        <div className="text-white text-center">
+          <p>
+            &copy;2025 <span className="font-oswald">CariStudio</span>. apiw. All Rights Reserved.
+          </p>
+        </div>
       </div>
     </footer>
   );
