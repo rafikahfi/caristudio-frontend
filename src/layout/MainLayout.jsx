f0eb249d022d35c5e49488bc602725c8dd0d20da
Rafi Kahfi Yugi <rafikahfi01@gmail.com>
2025-07-13 18:51:27 +0700

    Memperbaiki MainLayout.jsx

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 import "@radix-ui/themes/styles.css";
 function MainLayout() {
   return (
-    <main className="font-poppins 2xl:container mx-auto">
+    <main className="font-poppins">
       <Navbar />
-      <div className="px-6 pt-25 py-5 min-h-screen">
+      <div className="2xl:container mx-auto px-6 pt-25 py-5 min-h-screen">
         <Outlet />
       </div>
       <Footer />
